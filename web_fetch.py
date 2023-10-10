7621996c1a0d414dbc94b53e03b32018f2bb8e00
TomakataABC <60468929+TomakataABC@users.noreply.github.com>
2023-10-10 21:37:41 +0200

    BAllin vzhled změny

--- web_fetch.py
+++ web_fetch.py
@@ -4,30 +4,24 @@ import time
 
 from colorama import Fore, Style, Back
 
-header = f"{Fore.GREEN}======--- {Fore.BLUE}{Style.BRIGHT}{Back.LIGHTRED_EX} Ballin Program {Fore.GREEN}{Back.RESET} ---======"
-footer = f"{Fore.GREEN}------------------------------------"
-inputAsk = f"{Fore.BLUE}Vaše volba? {Fore.RESET}"
-invalidOption = f"{Back.RED}{Fore.BLACK} Neplatná volba {Back.RESET}{Fore.RESET}"
+header = f"{Fore.GREEN}======--- {Fore.BLUE}{Style.BRIGHT}{Back.LIGHTRED_EX} Ballin Program {Style.RESET_ALL}{Fore.GREEN} ---======"
+footer = f"{Fore.GREEN}------------------------------------{Style.RESET_ALL}"
+inputAsk = f"{Fore.BLUE}Vaše volba? {Style.RESET_ALL}"
+invalidOption = f"{Back.LIGHTRED_EX}{Fore.BLACK} Neplatná volba {Style.RESET_ALL}"
 
 
 def clear():
-    print("")
-    print("")
-    print("")
-    print("")
-    print("")
-    print("")
-    print("")
-    print("")
-    print("")
-    print("")
+    os.system('cls')
+    print('\n\n\n\n\n\n\n\n\n\n\n ')
+
 
 def mainMenu():
     clear()
     print(header)
-    print(f'        {Fore.BLUE}{Style.DIM}[{Style.BRIGHT}1{Style.DIM}] {Fore.LIGHTGREEN_EX}BF Data fetcher')
-    print(f'            {Fore.BLUE}{Style.DIM}[{Style.BRIGHT}2{Style.DIM}] {Fore.LIGHTGREEN_EX}BALLS?')
-    print(f'             {Fore.BLUE}{Style.DIM}[{Style.BRIGHT}3{Style.DIM}] {Fore.LIGHTGREEN_EX}Exit')
+    print('')
+    print(f'        {Fore.LIGHTBLUE_EX}{Style.DIM}[1]{Style.RESET_ALL} {Fore.LIGHTGREEN_EX}BF Data fetcher')
+    print(f'            {Fore.LIGHTBLUE_EX}{Style.DIM}[2]{Style.RESET_ALL} {Fore.LIGHTGREEN_EX}BALLS?')
+    print(f'             {Fore.LIGHTBLUE_EX}{Style.DIM}[3]{Style.RESET_ALL} {Fore.LIGHTGREEN_EX}Exit')
     print(footer)
 
     choice = int(input(inputAsk))
@@ -50,9 +44,12 @@ def mainMenu():
 def bfMenu():
     clear()
     print(header)
-    print(f'            {Fore.BLUE}{Style.DIM}[{Style.BRIGHT}1{Style.DIM}] {Fore.LIGHTGREEN_EX}Databáze')
-    print(f'             {Fore.BLUE}{Style.DIM}[{Style.BRIGHT}2{Style.DIM}] {Fore.LIGHTGREEN_EX}Služby')
-    print(f'              {Fore.BLUE}{Style.DIM}[{Style.BRIGHT}3{Style.DIM}] {Fore.LIGHTGREEN_EX}Zpět')
+    print('')
+    print(f'{Fore.MAGENTA}      ⮑ BF Data fetcher 3000 ⮐')
+    print('')
+    print(f'            {Fore.LIGHTBLUE_EX}{Style.DIM}[1]{Style.RESET_ALL} {Fore.LIGHTGREEN_EX}Databáze')
+    print(f'             {Fore.LIGHTBLUE_EX}{Style.DIM}[2]{Style.RESET_ALL} {Fore.LIGHTGREEN_EX}Služby')
+    print(f'              {Fore.LIGHTBLUE_EX}{Style.DIM}[3]{Style.RESET_ALL} {Fore.LIGHTGREEN_EX}Zpět')
     print(footer)
 
     choice = int(input(inputAsk))
@@ -72,7 +69,6 @@ def bfMenu():
         invalidChoice(bfMenu)
 
 
-
 def invalidChoice(menu):
     clear()
     print(invalidOption)
@@ -84,7 +80,6 @@ def exitProcess():
     clear()
     print(f"{Back.RED}{Fore.BLACK} Na viděnou :) {Back.RESET}{Fore.RESET}")
     sys.exit(0)
-
 
 
 mainMenu()
